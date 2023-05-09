ab7cb2e1abc4327f840f643ad2678eb5d6441f31
lbcanfield <lewie.b.canfield@gmail.com>
2023-05-09 05:51:01 -0500

    eventListener applied to the loading of the window.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './less/index.less'
 
 // Your code goes here!
+
+
+// load event
+
+window.addEventListener('load', function load(event) {
+     console.log(`${event.type} has executed`);
+});
+
